f9401108fda09891d39ac032e2a03d023fb78f02
wujimu <hasanseirafi69@gmail.com>
2022-12-05 14:49:11 -0800

    update server vars

--- config.js
+++ config.js
@@ -5,9 +5,16 @@ import dot from "dotenv";
  */
 export const database_tables = ["users", "logs"];
 
+
+let name = 'http://192.168.1.64:8080';
+if(process.env.PORT){
+    name = 'https://inventory2.onrender.com'
+}
+
+
 export const pivot_server = {
     //http://192.168.1.64:8080
-    name:"http://192.168.1.64:", 
+    name, 
 
     port:9000
 };
@@ -16,12 +23,13 @@ export function express_apps() {
 
     const env = dot.config().parsed;
 
-    const port = process.env.PORT || env.Port || 8080;
+ const port = process.env.PORT || env.Port || 8080;
+   
 
     const express_apps = new Map([ 
         [ "app_", 
         { 
-        name: "http://192.168.1.64:",
+        name,
          
         port : (port || 8080)
 
