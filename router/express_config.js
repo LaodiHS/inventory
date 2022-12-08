9896f3d06b85aea7acb45fca6fbf1d78a3f8e4b8
wujimu <hasanseirafi69@gmail.com>
2022-12-08 07:12:33 -0800

    remove https and secure attributes on client side cookies

--- router/express_config.js
+++ router/express_config.js
@@ -104,6 +104,8 @@ export default async function express_configuration(app) {
     session({
       secret: "secret123",
      // resave: true,
+     secure: false,
+     httpOnly:false,
       saveUninitialized: true,
       sameSite: 'lax'
       // cookie: {
